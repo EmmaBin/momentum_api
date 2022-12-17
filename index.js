85efde822f033556dd2f5cd64994c8674a00b49b
emmacodes <binma.emma@gmail.com>
2022-12-16 23:29:17 -0800

    used JS method, in its callback to get lat and lon to include inside fetch

--- index.js
+++ index.js
@@ -44,3 +44,21 @@ function getCurrentTime() {
 }
 
 setInterval(getCurrentTime, 1000)
+
+
+// a callback function,  from below method, it will return a promise of position
+// then we can manipulate with 'position', we use fetch to include the postion object information
+// that we needed in the query 
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => console.error(err))
+});
