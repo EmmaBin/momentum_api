c7bdee1998b30eaa0408a401840396412705836a
emmacodes <binma.emma@gmail.com>
2022-12-16 22:34:28 -0800

    add crypto img

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     )`
     document.getElementById('author').textContent = `By: ${data.user.name}`
 })
+
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        console.log(res.status)
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+    `
+    })
+    .catch(err => console.error(err))
